9e31b3344712e01523b152b1573dd7e50572d594
Jacky Lee <hklee310@gmail.com>
2019-08-01 01:20:52 -0400

    using fib instead of words

--- generate.py
+++ generate.py
@@ -1,45 +1,31 @@
 import os
 import random
-
+import copy
 # os.system('git add *')
 # os.system('git commit -m "added a word"')
 
-NUM_WORDS = 3
-OUTPUT_FILE = "hello.txt"
+OUTPUT_FILE = 'fib.txt'
+NUM_TO_GENERATE = 300;
 
 
-def issue_commands():
-    pass
+def _fib():
+    """ Generateor function """
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
 
-
-def _read_words(filename: str) -> list:
-    """Get words from a new line separated file"""
-    words = []
-    with open(filename) as file:
-        count = 0
-        for line in file.readlines():
-            count += 1
-            words.append(line)
-    return words
-
-
-def _get_random_word(words: list, num: int) -> str:
-    """"Return a list of n random elements from specified list"""
-    return [random.choice(words) for _ in range(num)]
-
-
-def _write_word(filename: str, word: str):
-    """Append a word to a file"""
-    os.system(f'echo {word} > {filename}')
-
-
-def main():
-    words = _read_words('words')
-    random_words = _get_random_word(words, NUM_WORDS)
-    for word in random_words:
-        _write_word(OUTPUT_FILE, word)
-
-    os.system('echo ezz > hello.txt')
 
 if __name__ == "__main__":
-    main()
+    fib_generator = _fib()
+
+    with open(OUTPUT_FILE, "a") as output_file:
+        for i in range(NUM_TO_GENERATE):
+            next_fib = next(fib_generator)
+            output_file.write(str(next_fib) + '\n')
+
+            # Make commit as of x date
+
+
+
+
